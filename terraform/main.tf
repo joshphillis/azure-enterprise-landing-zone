e891c52b6c6a596632df9b8866f9bfb4d6dfee3c
joshphillis <joshphillis@hotmail.com>
2026-01-06 16:53:09 -0600

    Refactor provider block in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-  sku_name  = "AZFW_VNet"
-  sku_tier  = "Standard"
-
-  ip_configuration {
-    name                 = "fw-ipconfig"
-    subnet_id            = "/subscriptions/8d3e1cd2-8996-4289-91db-4960cbdf5066/resourceGroups/EnterpriseLandingZone/providers/Microsoft.Network/virtualNetworks/hub-vnet/subnets/AzureFirewallSubnet"
-    public_ip_address_id = "/subscriptions/8d3e1cd2-8996-4289-91db-4960cbdf5066/resourceGroups/EnterpriseLandingZone/providers/Microsoft.Network/publicIPAddresses/fw-pip"
-  }
-
-}
