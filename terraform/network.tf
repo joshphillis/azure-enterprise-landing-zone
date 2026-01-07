d73acc28c4af11151699cb75bdd8426388b544a7
joshphillis <joshphillis@hotmail.com>
2026-01-07 09:12:06 -0600

    Refactor network resources in terraform/network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -10,6 +10,20 @@ resource "azurerm_virtual_network" "hub" {
   address_space       = [var.hub_vnet_address_space]
 }
 
+resource "azurerm_subnet" "default" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.enterprise.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "hub_default" {
+  name                 = "hub-default"
+  resource_group_name  = azurerm_resource_group.enterprise.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 resource "azurerm_subnet" "hub_firewall" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.enterprise.name
