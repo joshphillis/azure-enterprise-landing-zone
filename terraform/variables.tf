ec418de143991392b41af92d7e90949bd569b6cb
joshphillis <joshphillis@hotmail.com>
2026-01-08 13:13:56 -0600

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "location" {
+  description = "Azure region for the Enterprise Landing Zone resources."
+  type        = string
+  default     = "eastus"
+}
+
+variable "hub_vnet_address_space" {
+  description = "Address space for the hub virtual network."
+  type        = string
+  # TODO: Set to match your actual hub-vnet address space, e.g. "10.0.0.0/16"
+  default = "10.0.0.0/16"
+}
+
+variable "hub_firewall_subnet_prefix" {
+  description = "Address prefix for AzureFirewallSubnet."
+  type        = string
+  # TODO: Set to match your actual AzureFirewallSubnet prefix, e.g. "10.0.2.0/24"
+  default = "10.0.2.0/24"
+}
+
+variable "hub_firewall_mgmt_subnet_prefix" {
+  description = "Address prefix for AzureFirewallManagementSubnet."
+  type        = string
+  # TODO: Set to match your actual AzureFirewallManagementSubnet prefix, e.g. "10.0.3.0/24"
+  default = "10.0.3.0/24"
+}
