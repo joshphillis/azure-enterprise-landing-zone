e479c9671826c451868aac8d2507796a2cfe3137
Joshua Phillis <joshphillis@hotmail.com>
2026-01-08 12:31:57 -0600

    Trigger GitHub Actions deployment

--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-provider "azurerm" {
-  features {}
-
-  use_cli          = false
-  subscription_id  = var.subscription_id
-  tenant_id        = var.tenant_id
-  client_id        = var.client_id
-  client_secret    = var.client_secret
-}
