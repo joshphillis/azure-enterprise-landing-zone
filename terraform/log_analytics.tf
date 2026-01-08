f880be82189cdac97bed600d8ca90bd115e5d56d
Joshua Phillis <joshphillis@hotmail.com>
2026-01-08 13:46:26 -0600

    Fix log_analytics.tf merge conflict and restore valid resource block

--- terraform/log_analytics.tf
+++ terraform/log_analytics.tf
@@ -5,12 +5,4 @@ resource "azurerm_log_analytics_workspace" "hub" {
 
   sku               = "PerGB2018"
   retention_in_days = 30
-
-resource "azurerm_log_analytics_workspace" "hub" {
-  name                = "law-hub"
-  location            = azurerm_resource_group.enterprise.location
-  resource_group_name = azurerm_resource_group.enterprise.name
-
-  sku               = "PerGB2018"
-  retention_in_days = 30
 }
