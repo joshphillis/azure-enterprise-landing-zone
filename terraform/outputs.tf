8cf27387cde1b39f3205ed897b8b0f8588754fc9
joshphillis <joshphillis@hotmail.com>
2026-01-09 16:18:16 -0600

    Add files via upload

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,18 @@ output "log_analytics_workspace_id" {
   description = "ID of the Log Analytics workspace used for firewall diagnostics."
   value       = azurerm_log_analytics_workspace.hub.id
 }
+
+output "location" {
+  description = "Azure region for the Enterprise Landing Zone."
+  value       = azurerm_resource_group.enterprise.location
+}
+
+output "hub_default_subnet_id" {
+  description = "ID of the default subnet in the hub VNet."
+  value       = azurerm_subnet.default.id
+}
+
+output "hub_firewall_subnet_id" {
+  description = "ID of the AzureFirewallSubnet in the hub VNet."
+  value       = azurerm_subnet.hub_firewall.id
+}
