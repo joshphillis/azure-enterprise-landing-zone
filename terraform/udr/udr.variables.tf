6c4dc430aa4b2cd2b40977c946616638ffcabfe7
joshphillis <joshphillis@hotmail.com>
2026-01-10 12:36:34 -0600

    Add files via upload

---
+++ terraform/udr/udr.variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "default_subnet_id" {
+  type = string
+}
+
+variable "firewall_private_ip" {
+  type = string
+}
+
+variable "route_table_name" {
+  type = string
+}
