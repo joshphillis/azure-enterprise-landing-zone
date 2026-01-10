51fc8f5153db604475f71439188b0e5d61fae745
joshphillis <joshphillis@hotmail.com>
2026-01-10 12:30:02 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,12 +14,30 @@ provider "azurerm" {
 
 module "bastion" {
   source = "./bastion"
+
+  resource_group_name     = azurerm_resource_group.enterprise.name
+  location                = azurerm_resource_group.enterprise.location
+  virtual_network_name    = azurerm_virtual_network.hub.name
+  bastion_name            = var.bastion_name
+  bastion_subnet_name     = var.bastion_subnet_name
+  bastion_subnet_prefix   = "10.0.3.0/27"
 }
 
 module "nsg" {
   source = "./nsg"
+
+  resource_group_name = azurerm_resource_group.enterprise.name
+  location            = azurerm_resource_group.enterprise.location
+  default_subnet_id   = azurerm_subnet.default.id
+  nsg_name            = var.nsg_name
 }
 
 module "udr" {
   source = "./udr"
+
+  resource_group_name   = azurerm_resource_group.enterprise.name
+  location              = azurerm_resource_group.enterprise.location
+  default_subnet_id     = azurerm_subnet.default.id
+  firewall_private_ip   = azurerm_firewall.hub.ip_configuration[0].private_ip_address
+  route_table_name      = var.route_table_name
 }
