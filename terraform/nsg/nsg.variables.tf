ed069fef30d03b3f66520e22da95838b848532db
joshphillis <joshphillis@hotmail.com>
2026-01-10 12:35:45 -0600

    Add files via upload

---
+++ terraform/nsg/nsg.variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "default_subnet_id" {
+  type = string
+}
+
+variable "nsg_name" {
+  type = string
+}
