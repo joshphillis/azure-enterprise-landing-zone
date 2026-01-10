44391a061b0cd538456551c2641dafddda41119f
joshphillis <joshphillis@hotmail.com>
2026-01-10 12:34:26 -0600

    Add files via upload

---
+++ terraform/bastion/bastion.variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "virtual_network_name" {
+  type = string
+}
+
+variable "bastion_name" {
+  type = string
+}
+
+variable "bastion_subnet_name" {
+  type = string
+}
+
+variable "bastion_subnet_prefix" {
+  type = string
+}
