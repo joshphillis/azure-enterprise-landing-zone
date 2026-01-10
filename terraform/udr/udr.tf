cc5e649695936eb28faf3ac615cb5a50bb5f53fb
joshphillis <joshphillis@hotmail.com>
2026-01-10 12:40:06 -0600

    Update udr.tf

--- terraform/udr/udr.tf
+++ terraform/udr/udr.tf
@@ -1,19 +1,23 @@
 resource "azurerm_route_table" "default" {
   name                = var.route_table_name
-  location            = azurerm_resource_group.enterprise.location
-  resource_group_name = azurerm_resource_group.enterprise.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_route" "default_to_firewall" {
-  name                   = "DefaultToFirewall"
-  resource_group_name    = azurerm_resource_group.enterprise.name
+resource "azurerm_route" "default_route" {
+  name                   = "default-to-firewall"
+  resource_group_name    = var.resource_group_name
   route_table_name       = azurerm_route_table.default.name
   address_prefix         = "0.0.0.0/0"
   next_hop_type          = "VirtualAppliance"
-  next_hop_in_ip_address = azurerm_firewall.hub.ip_configuration[0].private_ip_address
+  next_hop_in_ip_address = var.firewall_private_ip
 }
 
 resource "azurerm_subnet_route_table_association" "default" {
-  subnet_id      = azurerm_subnet.default.id
+  subnet_id      = var.default_subnet_id
   route_table_id = azurerm_route_table.default.id
+}
+
+output "route_table_id" {
+  value = azurerm_route_table.default.id
 }
