8f037fd391b2bb307ed090582ee4baf3b96413a9
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:48:38 -0600

    Update udr.tf

--- terraform/udr/udr.tf
+++ terraform/udr/udr.tf
@@ -2,9 +2,10 @@ resource "azurerm_route_table" "default" {
   name                = var.route_table_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  tags                = var.tags
 }
 
-resource "azurerm_route" "default_route" {
+resource "azurerm_route" "default" {
   name                   = "default-to-firewall"
   resource_group_name    = var.resource_group_name
   route_table_name       = azurerm_route_table.default.name
@@ -16,8 +17,4 @@ resource "azurerm_route" "default_route" {
 resource "azurerm_subnet_route_table_association" "default" {
   subnet_id      = var.default_subnet_id
   route_table_id = azurerm_route_table.default.id
-}
-
-output "route_table_id" {
-  value = azurerm_route_table.default.id
 }
