3815955791c39f44edeff1d553ac2885ac222490
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:26:01 -0600

    Update firewall_policy.tf

--- terraform/firewall_policy.tf
+++ terraform/firewall_policy.tf
@@ -1,62 +1,26 @@
 resource "azurerm_firewall_policy" "hub" {
-  name                = "az-fw-hub"
+  name                = "${var.firewall_name}-policy"
   resource_group_name = azurerm_resource_group.enterprise.name
-  location            = azurerm_resource_group.enterprise.location
-
-  sku = "Standard"
+  location            = var.location
+  tags                = var.tags
 }
 
-resource "azurerm_firewall_policy_rule_collection_group" "hub_app_rules" {
-  name               = "rcg-app-rules"
+resource "azurerm_firewall_policy_rule_collection_group" "hub" {
+  name               = "default-rules"
   firewall_policy_id = azurerm_firewall_policy.hub.id
   priority           = 100
 
-  application_rule_collection {
-    name     = "allow-outbound-web"
+  network_rule_collection {
+    name     = "allow-internal"
     priority = 100
     action   = "Allow"
 
     rule {
-      name = "allow-http-https"
-
-      source_addresses = [
-        "10.0.0.0/8"
-      ]
-
-      destination_fqdns = [
-        "www.microsoft.com",
-        "learn.microsoft.com"
-      ]
-
-      protocols {
-        type = "Http"
-        port = 80
-      }
-
-      protocols {
-        type = "Https"
-        port = 443
-      }
-    }
-  }
-
-  network_rule_collection {
-    name     = "allow-dns"
-    priority = 200
-    action   = "Allow"
-
-    rule {
-      name = "dns-outbound"
-      source_addresses      = ["10.0.0.0/8"]
-      destination_ports     = ["53"]
-      destination_addresses = ["*"]
-      protocols             = ["UDP"]
+      name                  = "allow-vnet"
+      source_addresses      = [var.hub_vnet_cidr]
+      destination_addresses = [var.hub_vnet_cidr]
+      destination_ports     = ["*"]
+      protocols             = ["Any"]
     }
   }
 }
-
-# NOTE:
-# This is an example scaffold. Adjust or remove rules to match your actual policy.
-# If your existing policy is more complex, document it in README and selectively
-# migrate rules over time rather than trying to mirror it 1:1 on day one.
-
