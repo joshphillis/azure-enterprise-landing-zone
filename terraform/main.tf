78abdf4e8d4039b3a723ab646bd8d15cd06f7b74
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:26:57 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,42 @@
-provider "azurerm" {
-  features {}
+#############################################
+# Resource group and hub virtual network
+#############################################
 
-  use_cli          = false
-  subscription_id  = var.subscription_id
-  tenant_id        = var.tenant_id
-  client_id        = var.client_id
-  client_secret    = var.client_secret
+resource "azurerm_resource_group" "enterprise" {
+  name     = var.rg_name
+  location = var.location
+  tags     = var.tags
 }
 
-# ---------------------------------------------------------
-# NEW MODULES: Bastion, NSG, UDR
-# ---------------------------------------------------------
+resource "azurerm_virtual_network" "hub" {
+  name                = var.hub_vnet_name
+  address_space       = [var.hub_vnet_cidr]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.enterprise.name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = var.default_subnet_name
+  resource_group_name  = azurerm_resource_group.enterprise.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = [var.default_subnet_cidr]
+}
+
+#############################################
+# Modules: Bastion, NSG, UDR
+#############################################
 
 module "bastion" {
   source = "./bastion"
 
-  resource_group_name     = azurerm_resource_group.enterprise.name
-  location                = azurerm_resource_group.enterprise.location
-  virtual_network_name    = azurerm_virtual_network.hub.name
-  bastion_name            = var.bastion_name
-  bastion_subnet_name     = var.bastion_subnet_name
-  bastion_subnet_prefix   = var.bastion_subnet_prefix
+  resource_group_name   = azurerm_resource_group.enterprise.name
+  location              = azurerm_resource_group.enterprise.location
+  virtual_network_name  = azurerm_virtual_network.hub.name
+  bastion_name          = var.bastion_name
+  bastion_subnet_name   = var.bastion_subnet_name
+  bastion_subnet_prefix = var.bastion_subnet_cidr
+  tags                  = var.tags
 }
 
 module "nsg" {
@@ -30,6 +46,7 @@ module "nsg" {
   location            = azurerm_resource_group.enterprise.location
   default_subnet_id   = azurerm_subnet.default.id
   nsg_name            = var.nsg_name
+  tags                = var.tags
 }
 
 module "udr" {
@@ -40,4 +57,5 @@ module "udr" {
   default_subnet_id   = azurerm_subnet.default.id
   firewall_private_ip = azurerm_firewall.hub.ip_configuration[0].private_ip_address
   route_table_name    = var.route_table_name
+  tags                = var.tags
 }
