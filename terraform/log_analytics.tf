cac94ac2c78cede03402658eaab94eccad56df21
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:26:25 -0600

    Update log_analytics.tf

--- terraform/log_analytics.tf
+++ terraform/log_analytics.tf
@@ -1,8 +1,8 @@
 resource "azurerm_log_analytics_workspace" "hub" {
-  name                = "law-hub"
-  location            = azurerm_resource_group.enterprise.location
+  name                = var.log_analytics_name
+  location            = var.location
   resource_group_name = azurerm_resource_group.enterprise.name
-
-  sku               = "PerGB2018"
-  retention_in_days = 30
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = var.tags
 }
