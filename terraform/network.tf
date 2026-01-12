680b1acb10fd60a1724c82d3e29feddd26953ce7
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:27:20 -0600

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,39 +1,6 @@
-resource "azurerm_resource_group" "enterprise" {
-  name     = "EnterpriseLandingZone"
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "hub" {
-  name                = "hub-vnet"
-  location            = azurerm_resource_group.enterprise.location
-  resource_group_name = azurerm_resource_group.enterprise.name
-  address_space       = [var.hub_vnet_address_space]
-}
-
-resource "azurerm_subnet" "default" {
-  name                 = "default"
+resource "azurerm_subnet" "firewall" {
+  name                 = var.firewall_subnet_name
   resource_group_name  = azurerm_resource_group.enterprise.name
   virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes     = ["10.0.0.0/24"]
-}
-
-resource "azurerm_subnet" "hub_default" {
-  name                 = "hub-default"
-  resource_group_name  = azurerm_resource_group.enterprise.name
-  virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_subnet" "hub_firewall" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_resource_group.enterprise.name
-  virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes     = [var.hub_firewall_subnet_prefix]
-}
-
-resource "azurerm_subnet" "hub_firewall_mgmt" {
-  name                 = "AzureFirewallManagementSubnet"
-  resource_group_name  = azurerm_resource_group.enterprise.name
-  virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes     = [var.hub_firewall_mgmt_subnet_prefix]
+  address_prefixes     = [var.firewall_subnet_cidr]
 }
