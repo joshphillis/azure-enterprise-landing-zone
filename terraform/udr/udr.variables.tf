5b3f00d317cb6d7d7293163f08df2f4f43a7880e
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:49:01 -0600

    Update udr.variables.tf

--- terraform/udr/udr.variables.tf
+++ terraform/udr/udr.variables.tf
@@ -1,19 +1,6 @@
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "default_subnet_id" {
-  type = string
-}
-
-variable "firewall_private_ip" {
-  type = string
-}
-
-variable "route_table_name" {
-  type = string
-}
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "default_subnet_id"   { type = string }
+variable "firewall_private_ip" { type = string }
+variable "route_table_name"    { type = string }
+variable "tags"                { type = map(string) }
