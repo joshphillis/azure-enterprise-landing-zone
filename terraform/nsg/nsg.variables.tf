dfbef34d43e1fdaa7efe975365a8bde893ae6d4c
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:48:11 -0600

    Update nsg.variables.tf

--- terraform/nsg/nsg.variables.tf
+++ terraform/nsg/nsg.variables.tf
@@ -1,15 +1,5 @@
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "default_subnet_id" {
-  type = string
-}
-
-variable "nsg_name" {
-  type = string
-}
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "default_subnet_id"   { type = string }
+variable "nsg_name"            { type = string }
+variable "tags"                { type = map(string) }
