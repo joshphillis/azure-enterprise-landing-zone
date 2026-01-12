93236846d78f25aefa5bf3cb0d58274052f9b6d4
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:47:19 -0600

    Update bastion.variables.tf

--- terraform/bastion/bastion.variables.tf
+++ terraform/bastion/bastion.variables.tf
@@ -1,23 +1,7 @@
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "virtual_network_name" {
-  type = string
-}
-
-variable "bastion_name" {
-  type = string
-}
-
-variable "bastion_subnet_name" {
-  type = string
-}
-
-variable "bastion_subnet_prefix" {
-  type = string
-}
+variable "resource_group_name"   { type = string }
+variable "location"              { type = string }
+variable "virtual_network_name"  { type = string }
+variable "bastion_name"          { type = string }
+variable "bastion_subnet_name"   { type = string }
+variable "bastion_subnet_prefix" { type = string }
+variable "tags"                  { type = map(string) }
