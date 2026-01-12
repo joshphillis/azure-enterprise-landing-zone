db48465ff0fed0c6a46aa03c800b86e0fc7d7a03
joshphillis <joshphillis@hotmail.com>
2026-01-12 13:29:15 -0600

    Update public_ips.tf

--- terraform/public_ips.tf
+++ terraform/public_ips.tf
@@ -1,17 +1,8 @@
-resource "azurerm_public_ip" "azfw_pip" {
-  name                = "azfw-pip"
-  location            = azurerm_resource_group.enterprise.location
+resource "azurerm_public_ip" "firewall" {
+  name                = "${var.firewall_name}-pip"
+  location            = var.location
   resource_group_name = azurerm_resource_group.enterprise.name
-
-  allocation_method = "Static"
-  sku               = "Standard"
-}
-
-resource "azurerm_public_ip" "azfw_mgmt_pip" {
-  name                = "azfw-mgmt-pip"
-  location            = azurerm_resource_group.enterprise.location
-  resource_group_name = azurerm_resource_group.enterprise.name
-
-  allocation_method = "Static"
-  sku               = "Standard"
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
 }
